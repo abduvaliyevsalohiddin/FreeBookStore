716279663c2f8402cacc7354dc104bf0ea0c44a2
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-03-17 02:55:59 -0700

    media refresh

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView, )
@@ -27,5 +29,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
